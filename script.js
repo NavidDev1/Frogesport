6fc1a1efbf332f53510b678440fad8026a0417ed
Naviddev1 <Navid86@hotmail.se>
2022-10-11 13:02:39 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const startBtn = document.querySelector('startaBtn');
+const yesBtn = document.querySelector('yesBtn');
+const noBtn = document.querySelector('noBtn');
+const pictureBox = document.querySelector('pictureBox');
+const pictureDiv = document.querySelector('pictureDiv');
+const question = document.querySelector('question');
+
+/* när man kommer in på sidan vill vi se start knappem
+rätt och fel knapparna syns ej.
+vid starta spelet click så ändras rubriken till frågor
+rätt och fel knapparna syns
+bilden ändras.
+vi vill att det ska finns två arrays som räknar antal rätt och fel svar*/
